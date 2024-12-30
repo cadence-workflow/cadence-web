b9ebd617c457d82301f5965b969045c7bfe72e28
Adhitya Mamallan <adhitya.mamallan@uber.com>
2024-12-30 14:31:49 +0000

    Update unit tests

--- src/views/domain-workflows-archival/domain-workflows-archival-header/__tests__/domain-workflows-archival-header.test.tsx
+++ src/views/domain-workflows-archival/domain-workflows-archival-header/__tests__/domain-workflows-archival-header.test.tsx
@@ -41,13 +41,9 @@ describe(DomainWorkflowsArchivalHeader.name, () => {
       />
     );
 
-<<<<<<< HEAD
     expect(mockSetQueryParams).toHaveBeenCalledWith({
       timeRangeStartArchival: '2023-05-15T00:00:00.000Z',
       timeRangeEndArchival: '2023-05-25T00:00:00.000Z',
     });
-=======
-    expect(mockSetQueryParams).toHaveBeenCalledWith('test');
->>>>>>> 4760a65 (Add archival view)
   });
 });
