da585f4641c109d451e954999099856eb2837831
Adhitya Mamallan <adhitya.mamallan@uber.com>
2025-03-11 16:32:19 +0100

    Remove unnecessary comment

--- src/views/domain-workflows-basic/hooks/helpers/should-pick-second-workflow.ts
+++ src/views/domain-workflows-basic/hooks/helpers/should-pick-second-workflow.ts
@@ -3,7 +3,6 @@ import { type DomainWorkflow } from '@/views/domain-page/domain-page.types';
 /**
  * Since ListOpenWorkflows returns workflows sorted by their start time, and ListClosedWorkflows sorts them by their
  * close time, we cannot merge them. Therefore, the sorting logic prioritizes open workflows over closed workflows.
- * Returns true if the second workflow should be picked over the first workflow.
  */
 export default function shouldPickSecondWorkflow(
   first: DomainWorkflow,
