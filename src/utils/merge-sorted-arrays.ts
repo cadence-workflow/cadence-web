e205e9c6aad33de4c413a9ff54580133d3f3e070
Adhitya Mamallan <adhitya.mamallan@uber.com>
2025-03-12 15:39:34 +0100

    Update docstrings

--- src/utils/merge-sorted-arrays.ts
+++ src/utils/merge-sorted-arrays.ts
@@ -8,9 +8,10 @@
  * @param sortedArrays - An array of sorted arrays to pick items from and merge.
  * @param itemsCount - The number of items to pick from the sorted arrays.
  * @param compareFunc - A comparison function used to determine the order of items.
- *   - If the function returns a number > 0, the first argument is considered the higher-priority candidate.
- *   - If the function returns a number <= 0, the second argument is considered the higher-priority candidate.
- * **Note:** The comparison logic must align with the sorting logic of the input arrays to maintain consistency.
+ *   - If the function returns a positive number (> 0), the second argument is considered the higher-priority candidate.
+ *   - If the function returns a negative number (<= 0), the first argument is considered the higher-priority candidate.
+ *   - The comparison logic must align with the sorting logic of the input arrays to maintain consistency.
+ *   - For more details, see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort#comparefn
  *
  * @returns An object containing:
  *   - `pointers`: An array of the last picked index from each input array.
