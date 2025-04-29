a929504abe086019bc986b21bcfbf20903cdd918
Assem Hafez <assem.hafez@uber.com>
2025-04-29 07:46:57 +0000

    swap function position

--- src/utils/data-formatters/format-input-payload.ts
+++ src/utils/data-formatters/format-input-payload.ts
@@ -16,45 +16,6 @@ const formatInputPayload = (
   return parseMultipleInputs(parsedData);
 };
 
-const extractJsonValue = (input: string, startIndex: number) => {
-  let endIndex = startIndex;
-  let openBrackets = 0;
-  let openBraces = 0;
-  let inString = false;
-  let escapeNext = false; // used to handle escaped quotes
-
-  while (endIndex < input.length) {
-    const char = input[endIndex];
-
-    if (escapeNext) {
-      escapeNext = false;
-    } else if (char === '\\') {
-      escapeNext = true;
-    } else if (char === '"' && !inString) {
-      inString = true;
-    } else if (char === '"' && inString) {
-      inString = false;
-    } else if (!inString) {
-      if (char === '[') openBrackets++;
-      if (char === ']') openBrackets--;
-      if (char === '{') openBraces++;
-      if (char === '}') openBraces--;
-
-      // When a separator is found, this indicates the end of a JSON value if we are not inside any array or object
-      if (separators.includes(char) && openBrackets === 0 && openBraces === 0) {
-        break;
-      }
-    }
-    endIndex++;
-  }
-
-  return {
-    startIndex,
-    endIndex,
-    jsonString: input.slice(startIndex, endIndex),
-  };
-};
-
 const parseMultipleInputs = (input: string) => {
   const results = [];
   let currentIndex = 0;
@@ -93,4 +54,42 @@ const parseMultipleInputs = (input: string) => {
   return results;
 };
 
+const extractJsonValue = (input: string, startIndex: number) => {
+  let endIndex = startIndex;
+  let openBrackets = 0;
+  let openBraces = 0;
+  let inString = false;
+  let escapeNext = false; // used to handle escaped quotes
+
+  while (endIndex < input.length) {
+    const char = input[endIndex];
+
+    if (escapeNext) {
+      escapeNext = false;
+    } else if (char === '\\') {
+      escapeNext = true;
+    } else if (char === '"' && !inString) {
+      inString = true;
+    } else if (char === '"' && inString) {
+      inString = false;
+    } else if (!inString) {
+      if (char === '[') openBrackets++;
+      if (char === ']') openBrackets--;
+      if (char === '{') openBraces++;
+      if (char === '}') openBraces--;
+
+      // When a separator is found, this indicates the end of a JSON value if we are not inside any array or object
+      if (separators.includes(char) && openBrackets === 0 && openBraces === 0) {
+        break;
+      }
+    }
+    endIndex++;
+  }
+
+  return {
+    startIndex,
+    endIndex,
+    jsonString: input.slice(startIndex, endIndex),
+  };
+};
 export default formatInputPayload;
