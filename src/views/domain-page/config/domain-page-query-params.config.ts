d7ef25b1ba7e5f7a941d651a9254d2bafeaad2b7
Adhitya Mamallan <adhitya.mamallan@uber.com>
2025-05-14 14:26:00 +0200

    Add new date filter for archival

--- src/views/domain-page/config/domain-page-query-params.config.ts
+++ src/views/domain-page/config/domain-page-query-params.config.ts
@@ -4,18 +4,13 @@ import {
   type PageQueryParamMultiValue,
   type PageQueryParam,
 } from '@/hooks/use-page-query-params/use-page-query-params.types';
-import dayjs from '@/utils/datetime/dayjs';
-import parseDateQueryParam from '@/utils/datetime/parse-date-query-param';
 import { type SortOrder } from '@/utils/sort-by';
-import DOMAIN_WORKFLOWS_ARCHIVAL_START_DAYS_CONFIG from '@/views/domain-workflows-archival/config/domain-workflows-archival-start-days.config';
 import { type WorkflowStatusClosed } from '@/views/domain-workflows-archival/domain-workflows-archival-header/domain-workflows-archival-header.types';
 import { type WorkflowStatusBasicVisibility } from '@/views/domain-workflows-basic/domain-workflows-basic-filters/domain-workflows-basic-filters.types';
 import isWorkflowStatusBasicVisibility from '@/views/domain-workflows-basic/domain-workflows-basic-filters/helpers/is-workflow-status-basic-visibility';
 import isWorkflowStatus from '@/views/shared/workflow-status-tag/helpers/is-workflow-status';
 import { type WorkflowStatus } from '@/views/shared/workflow-status-tag/workflow-status-tag.types';
 import { type WorkflowsHeaderInputType } from '@/views/shared/workflows-header/workflows-header.types';
-
-const now = dayjs();
 
 const domainPageQueryParamsConfig: [
   PageQueryParam<'inputType', WorkflowsHeaderInputType>,
@@ -41,8 +36,8 @@ const domainPageQueryParamsConfig: [
     'statusesArchival',
     Array<WorkflowStatusClosed> | undefined
   >,
-  PageQueryParam<'timeRangeStartArchival', Date>,
-  PageQueryParam<'timeRangeEndArchival', Date>,
+  PageQueryParam<'timeRangeStartArchival', DateFilterValue>,
+  PageQueryParam<'timeRangeEndArchival', DateFilterValue>,
   PageQueryParam<'sortColumnArchival', string>,
   PageQueryParam<'sortOrderArchival', SortOrder>,
   PageQueryParam<'queryArchival', string>,
@@ -144,20 +139,14 @@ const domainPageQueryParamsConfig: [
   {
     key: 'timeRangeStartArchival',
     queryParamKey: 'astart',
-    defaultValue: now
-      .subtract(DOMAIN_WORKFLOWS_ARCHIVAL_START_DAYS_CONFIG, 'days')
-      .toDate(),
-    parseValue: (v) =>
-      parseDateQueryParam(v) ??
-      now
-        .subtract(DOMAIN_WORKFLOWS_ARCHIVAL_START_DAYS_CONFIG, 'days')
-        .toDate(),
+    defaultValue: 'now-7d',
+    parseValue: (v) => parseDateFilterValue(v, 'now-7d'),
   },
   {
     key: 'timeRangeEndArchival',
     queryParamKey: 'aend',
-    defaultValue: now.toDate(),
-    parseValue: (v) => parseDateQueryParam(v) ?? now.toDate(),
+    defaultValue: 'now',
+    parseValue: (v) => parseDateFilterValue(v, 'now'),
   },
   {
     key: 'sortColumnArchival',
