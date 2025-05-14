8dba2478450d71c74788721b8ac1e0c5b3556d9b
Assem Hafez <assem.hafez@uber.com>
2025-05-14 15:53:49 +0000

    Signal workflow form

---
+++ src/views/workflow-actions/workflow-action-signal-form/workflow-action-signal-form.tsx
@@ -0,0 +1,66 @@
+import { FormControl } from 'baseui/form-control';
+import { Input } from 'baseui/input';
+import { Textarea } from 'baseui/textarea';
+import { Controller } from 'react-hook-form';
+
+import { type WorkflowActionFormProps } from '../workflow-actions.types';
+
+import { type SignalWorkflowFormData } from './workflow-action-signal-form.types';
+
+export default function WorkflowActionSignalForm({
+  fieldErrors,
+  control,
+  formData,
+}: WorkflowActionFormProps<SignalWorkflowFormData>) {
+  const getErrorMessage = (field: string) => {
+    return field in fieldErrors
+      ? fieldErrors[field as keyof typeof fieldErrors]?.message
+      : undefined;
+  };
+
+  return (
+    <div>
+      <FormControl label="Signal Name">
+        <Controller
+          name="signalName"
+          control={control}
+          defaultValue=""
+          render={({ field: { ref, ...field } }) => (
+            <Input
+              {...field}
+              // @ts-expect-error - inputRef expects ref object while ref is a callback. It should support both.
+              inputRef={ref}
+              onChange={(e) => {
+                field.onChange(e.target.value);
+              }}
+              onBlur={field.onBlur}
+              error={Boolean(getErrorMessage('signalName'))}
+              placeholder="Enter signal name"
+            />
+          )}
+        />
+      </FormControl>
+
+      <FormControl label="JSON Input (optional)">
+        <Controller
+          name="signalInput"
+          control={control}
+          defaultValue=""
+          render={({ field: { ref, ...field } }) => (
+            <Textarea
+              {...field}
+              // @ts-expect-error - inputRef expects ref object while ref is a callback. It should support both.
+              inputRef={ref}
+              onChange={(e) => {
+                field.onChange(e.target.value);
+              }}
+              onBlur={field.onBlur}
+              error={Boolean(getErrorMessage('signalInput'))}
+              placeholder="Enter valid JSON input"
+            />
+          )}
+        />
+      </FormControl>
+    </div>
+  );
+}
