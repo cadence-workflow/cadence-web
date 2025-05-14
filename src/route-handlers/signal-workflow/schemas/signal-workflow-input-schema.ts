6b6ce059fe9ced21e7f872801241c6b11e43e7f5
Assem Hafez <assem.hafez@uber.com>
2025-05-14 13:12:14 +0000

    Signal workflow API

---
+++ src/route-handlers/signal-workflow/schemas/signal-workflow-input-schema.ts
@@ -0,0 +1,15 @@
+import losslessJsonParse from "@/utils/lossless-json-parse";
+import { z } from "zod";
+
+const signalWorkflowInputSchema = z.string().superRefine((str, ctx) => {
+  if (!str) return undefined;
+
+  try {
+    return losslessJsonParse(str);
+  } catch {
+    ctx.addIssue({ code: 'custom', message: 'Invalid JSON' });
+    return z.NEVER;
+  }
+});
+
+export default signalWorkflowInputSchema;
---
+++ src/route-handlers/signal-workflow/signal-workflow.types.ts
@@ -0,0 +1,14 @@
+import { type GRPCClusterMethods } from '@/utils/grpc/grpc-client';
+
+export type RequestParams = {
+  params: {
+    domain: string;
+    cluster: string;
+    workflowId: string;
+    runId: string;
+  };
+};
+
+export type Context = {
+  grpcClusterMethods: GRPCClusterMethods;
+}; 
