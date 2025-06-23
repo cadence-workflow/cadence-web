1711cbfd92179174e3cda8a994b9e9084e48d01c
Adhitya Mamallan <adhitya.mamallan@uber.com>
2025-06-23 11:42:44 +0200

    Clean up after rebase

--- src/views/workflow-history/config/workflow-history-ungrouped-grid-template-columns.config.ts
+++
@@ -1,5 +0,0 @@
-// ID, event type, event, time, elapsed duration, badges
-const WORKFLOW_HISTORY_UNGROUPED_GRID_TEMPLATE_COLUMNS_CONFIG =
-  '0.3fr 2fr 1.3fr 1.2fr 1fr 1fr';
-
-export default WORKFLOW_HISTORY_UNGROUPED_GRID_TEMPLATE_COLUMNS_CONFIG;
