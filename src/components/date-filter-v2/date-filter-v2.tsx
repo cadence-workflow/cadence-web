432012d51749da0b2106771d7a404e1d4229bc15
Adhitya Mamallan <adhitya.mamallan@uber.com>
2025-06-23 14:45:21 +0200

    Fix string

--- src/components/date-filter-v2/date-filter-v2.tsx
+++ src/components/date-filter-v2/date-filter-v2.tsx
@@ -82,7 +82,7 @@ export default function DateFilterV2({
   );
 
   const displayValue = useMemo<string | undefined>(() => {
-    if (!dates.end || !dates.start) return undefined;
+    if (!dates.end || !dates.start) return '';
     if (dates.end === 'now' && isRelativeDateFilterValue(dates.start))
       return DATE_FILTER_RELATIVE_VALUES[dates.start].label;
 
