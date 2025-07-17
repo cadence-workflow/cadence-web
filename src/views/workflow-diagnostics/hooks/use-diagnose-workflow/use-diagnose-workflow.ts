47030d4a151be1302677570a4ebbc0ab3a0ec563
Adhitya Mamallan <adhitya.mamallan@uber.com>
2025-07-17 10:27:06 +0200

    remove comment

--- src/views/workflow-diagnostics/hooks/use-diagnose-workflow/use-diagnose-workflow.ts
+++ src/views/workflow-diagnostics/hooks/use-diagnose-workflow/use-diagnose-workflow.ts
@@ -4,10 +4,5 @@ import getDiagnoseWorkflowQueryOptions from './get-diagnose-workflow-query-optio
 import { type UseDiagnoseWorkflowParams } from './use-diagnose-workflow.types';
 
 export default function useDiagnoseWorkflow(params: UseDiagnoseWorkflowParams) {
-  /**
-   * We intentionally use a regular (non-suspense) query for diagnostics because:
-   * - The hook will be invoked in multiple locations, so we want to leverage the query cache.
-   * - Diagnostics can take a while to complete, and we don't want to block the UI while waiting.
-   */
   return useQuery(getDiagnoseWorkflowQueryOptions(params));
 }
