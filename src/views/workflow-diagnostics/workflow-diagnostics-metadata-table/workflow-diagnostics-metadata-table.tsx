67aa91d53f2122ccbeccc52668404f47ee8c472d
Adhitya Mamallan <adhitya.mamallan@uber.com>
2025-07-23 17:21:09 +0200

    Implement metadata table

--- src/views/workflow-diagnostics/workflow-diagnostics-metadata-table/workflow-diagnostics-metadata-table.tsx
+++ src/views/workflow-diagnostics/workflow-diagnostics-metadata-table/workflow-diagnostics-metadata-table.tsx
@@ -1,5 +1,25 @@
+import parseWorkflowDiagnosticsMetadata from './helpers/parse-workflow-diagnostics-metadata';
+import { styled } from './workflow-diagnostics-metadata-table.styles';
 import { type Props } from './workflow-diagnostics-metadata-table.types';
 
 export default function WorkflowDiagnosticsMetadataTable(props: Props) {
-  return <div>{JSON.stringify(props.metadata, null, 2)}</div>;
+  const parsedMetadataItems = parseWorkflowDiagnosticsMetadata(props);
+
+  return (
+    <styled.MetadataTableContainer>
+      {parsedMetadataItems.map((metadataItem) => (
+        <styled.MetadataItemRow
+          $forceWrap={metadataItem.forceWrap}
+          key={metadataItem.key}
+        >
+          <styled.MetadataItemLabel $forceWrap={metadataItem.forceWrap}>
+            {metadataItem.label}
+          </styled.MetadataItemLabel>
+          <styled.MetadataItemValue>
+            {metadataItem.value}
+          </styled.MetadataItemValue>
+        </styled.MetadataItemRow>
+      ))}
+    </styled.MetadataTableContainer>
+  );
 }
--- src/views/workflow-diagnostics/workflow-diagnostics-metadata-table/workflow-diagnostics-metadata-table.types.ts
+++ src/views/workflow-diagnostics/workflow-diagnostics-metadata-table/workflow-diagnostics-metadata-table.types.ts
@@ -1,5 +1,25 @@
+import { type ComponentType } from 'react';
+
 import { type WorkflowPageParams } from '@/views/workflow-page/workflow-page.types';
 
 export type Props = {
   metadata: any;
 } & WorkflowPageParams;
+
+export type MetadataValueComponentProps = {
+  value: any;
+} & WorkflowPageParams;
+
+export type WorkflowDiagnosticsMetadataParser = {
+  name: string;
+  matcher: (key: string, value: unknown) => boolean;
+  renderValue: ComponentType<MetadataValueComponentProps>;
+  forceWrap?: boolean;
+};
+
+export type ParsedWorkflowDiagnosticsMetadataField = {
+  key: string;
+  label: string;
+  forceWrap?: boolean;
+  value: React.ReactNode;
+};
