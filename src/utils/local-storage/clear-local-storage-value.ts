d992e4ed6dc87e4f03d9bb6cc9ccd65edfb5a81f
Adhitya Mamallan <adhitya.mamallan@uber.com>
2025-07-30 13:31:02 +0200

    Add exception handling to local storage methods

--- src/utils/local-storage/clear-local-storage-value.ts
+++ src/utils/local-storage/clear-local-storage-value.ts
@@ -1,4 +1,11 @@
+import logger from '../logger';
+
 export default function clearLocalStorageValue(key: string) {
   if (typeof window === 'undefined') return;
-  localStorage.removeItem(key);
+
+  try {
+    return localStorage.removeItem(key);
+  } catch (error) {
+    logger.warn({ key, error }, 'Failed to clear value from local storage');
+  }
 }
--- src/utils/local-storage/get-local-storage-value.ts
+++ src/utils/local-storage/get-local-storage-value.ts
@@ -1,5 +1,12 @@
+import logger from '../logger';
+
 export default function getLocalStorageValue(key: string) {
   if (typeof window === 'undefined') return null;
 
-  return localStorage.getItem(key);
+  try {
+    return localStorage.getItem(key);
+  } catch (error) {
+    logger.warn({ key, error }, 'Failed to get value from local storage');
+    return null;
+  }
 }
