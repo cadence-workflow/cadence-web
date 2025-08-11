7049e3c8769785e1aedc15a7ab5b380bb84f8185
Assem Hafez <assem.hafez@uber.com>
2025-08-11 11:58:41 +0000

    add useSuspenseConfigValue hook

--- src/hooks/use-config-value/use-config-value.types.ts
+++ src/hooks/use-config-value/use-config-value.types.ts
@@ -1,12 +1,33 @@
-import { type UseQueryResult } from '@tanstack/react-query';
+import {
+  type UseQueryOptions,
+  type UseQueryResult,
+  type UseSuspenseQueryResult,
+} from '@tanstack/react-query';
 
 import {
+  type GetConfigRequestQuery,
+  type GetConfigArgs,
   type GetConfigKeys,
   type GetConfigResponse,
 } from '@/route-handlers/get-config/get-config.types';
+import { type PublicDynamicConfigKeys } from '@/utils/config/config.types';
 import { type RequestError } from '@/utils/request/request-error';
 
 export type UseConfigValueResult<K extends GetConfigKeys> = UseQueryResult<
   GetConfigResponse<K>,
   RequestError
+>;
+export type UseSuspenseConfigValueResult<K extends GetConfigKeys> =
+  UseSuspenseQueryResult<GetConfigResponse<K>, RequestError>;
+
+export type UseConfigValueParams<K extends PublicDynamicConfigKeys> = {
+  key: K;
+  args?: GetConfigArgs<K>;
+};
+
+export type UseConfigQueryOptions<K extends GetConfigKeys> = UseQueryOptions<
+  GetConfigResponse<K>,
+  RequestError,
+  GetConfigResponse<K>,
+  [string, GetConfigRequestQuery<K>]
 >;
