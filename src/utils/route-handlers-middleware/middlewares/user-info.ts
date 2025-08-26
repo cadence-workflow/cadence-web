895a40ea869feb1351b4704746294d339c53ead7
Assem Hafez <137278762+Assem-Uber@users.noreply.github.com>
2025-08-26 16:39:59 +0200

    Update src/utils/route-handlers-middleware/middlewares/user-info.ts
    Co-authored-by: Adhitya Mamallan <adi1998is@gmail.com>

--- src/utils/route-handlers-middleware/middlewares/user-info.ts
+++ src/utils/route-handlers-middleware/middlewares/user-info.ts
@@ -5,7 +5,7 @@ import { type UserInfoMiddlewareContext } from './user-info.types';
 const userInfo: MiddlewareFunction<
   ['userInfo', UserInfoMiddlewareContext]
 > = async () => {
-  // Placeholder for user info middleware, to be implemented after intergrating auth
+  // Placeholder for user info middleware, to be implemented after integrating auth
   return ['userInfo', null];
 };
 
