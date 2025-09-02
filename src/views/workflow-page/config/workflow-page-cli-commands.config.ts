52f25487920c1a505c70e56e2baf7a47e4a7830d
Yoav Levy <yoavlev2@gmail.com>
2025-09-02 11:41:56 +0200

    Added cancel/terminate cli commands. Also made sure the modal stays withing screen bounderies and commands are scrollable

--- src/views/workflow-page/config/workflow-page-cli-commands.config.ts
+++ src/views/workflow-page/config/workflow-page-cli-commands.config.ts
@@ -90,14 +90,18 @@ const workflowPageCliCommandsConfig: CliCommandConfig[] = [
     label: 'Cancel a workflow',
     command:
       'cadence --env {staging|prod|(others)} --proxy_region {dca|phx} --domain {domain-name} workflow cancel -w {workflow-id} -r {run-id} --reason {reason details}',
+    description:
+      'Cancel a workflow, recording WorkflowExecutionCancelRequested event and allowing for clean up work after cancellation',
     group: 'workflow',
-},
-{
+  },
+  {
     label: 'Terminate a workflow',
     command:
       'cadence --env {staging|prod|(others)} --proxy_region {dca|phx} --domain {domain-name} workflow terminate -w {workflow-id} -r {run-id} --reason {reason details}',
+    description:
+      'Terminate a workflow immediately, recording WorkflowExecutionTerminated event as the closing event in the history',
     group: 'workflow',
-},
+  },
 ];
 
 export default workflowPageCliCommandsConfig;
