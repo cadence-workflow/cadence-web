3e027179bf14a03848e690132d97cf580c8347de
Assem Hafez <assem.hafez@uber.com>
2025-11-03 11:41:28 +0100

    Create hook for fetching history
    Signed-off-by: Assem Hafez <assem.hafez@uber.com>

--- src/views/workflow-history/hooks/use-workflow-history-fetcher.ts
+++ src/views/workflow-history/hooks/use-workflow-history-fetcher.ts
@@ -57,7 +57,7 @@ export default function useWorkflowHistoryFetcher(
 
   useEffect(() => {
     return () => {
-      fetcherRef.current?.unmount();
+      fetcherRef.current?.destroy();
     };
   }, []);
 
