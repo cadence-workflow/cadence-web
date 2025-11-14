fc48a814aa4748aa68c3345284bd00794c917153
Adhitya Mamallan <adhitya.mamallan@uber.com>
2025-11-14 18:37:59 +0100

    delete modal
    Signed-off-by: Adhitya Mamallan <adhitya.mamallan@uber.com>

--- src/views/domain-page/domain-page-failover-modal/domain-page-failover-modal.types.ts
+++
@@ -1,7 +0,0 @@
-import { type FailoverEvent } from '@/route-handlers/list-failover-history/list-failover-history.types';
-
-export type Props = {
-  failoverEvent: FailoverEvent;
-  isOpen: boolean;
-  onClose: () => void;
-};
