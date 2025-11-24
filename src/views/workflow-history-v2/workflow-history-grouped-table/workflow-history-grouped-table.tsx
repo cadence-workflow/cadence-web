cd6517141b0d2186175d3cf191ec6ca3dd4e8a3d
Adhitya Mamallan <adhitya.mamallan@uber.com>
2025-11-24 15:37:06 +0100

    Add grouped table with raw JSON of events
    Signed-off-by: Adhitya Mamallan <adhitya.mamallan@uber.com>

--- src/views/workflow-history-v2/workflow-history-grouped-table/workflow-history-grouped-table.tsx
+++ src/views/workflow-history-v2/workflow-history-grouped-table/workflow-history-grouped-table.tsx
@@ -1,20 +1,82 @@
-import { styled } from './workflow-history-grouped-table.styles';
+import { Virtuoso } from 'react-virtuoso';
 
-/**
- * To be used in History v2
- */
-export default function WorkflowHistoryGroupedTable() {
+import WorkflowHistoryTimelineLoadMore from '@/views/workflow-history/workflow-history-timeline-load-more/workflow-history-timeline-load-more';
+
+import { styled } from './workflow-history-grouped-table.styles';
+import { type Props } from './workflow-history-grouped-table.types';
+
+export default function WorkflowHistoryGroupedTable({
+  eventGroupsById,
+  virtuosoRef,
+  initialStartIndex,
+  setVisibleRange,
+  error,
+  hasMoreEvents,
+  fetchMoreEvents,
+  isFetchingMoreEvents,
+}: Props) {
   return (
     <>
       <styled.TableHeader>
-        <div>ID</div>
+        <div />
         <div>Event group</div>
         <div>Status</div>
         <div>Time</div>
         <div>Duration</div>
         <div>Details</div>
       </styled.TableHeader>
-      {/* TODO @adhityamamallan: Add table body with Virtuoso  with new design*/}
+      <Virtuoso
+        useWindowScroll
+        data={eventGroupsById}
+        ref={virtuosoRef}
+        defaultItemHeight={160}
+        rangeChanged={setVisibleRange}
+        {...(initialStartIndex === undefined
+          ? {}
+          : {
+              initialTopMostItemIndex: {
+                index: initialStartIndex,
+                align: 'start',
+                behavior: 'auto',
+              },
+            })}
+        itemContent={(_, [__, group]) => (
+          <div>{JSON.stringify(group)}</div>
+          // <WorkflowHistoryTimelineGroup
+          //   key={groupId}
+          //   {...group}
+          //   showLoadingMoreEvents={
+          //     group.hasMissingEvents && !reachedEndOfAvailableHistory
+          //   }
+          //   resetToDecisionEventId={group.resetToDecisionEventId}
+          //   isLastEvent={index === filteredEventGroupsEntries.length - 1}
+          //   decodedPageUrlParams={decodedParams}
+          //   getIsEventExpanded={getIsEventExpanded}
+          //   onEventToggle={toggleIsEventExpanded}
+          //   onReset={() => {
+          //     if (group.resetToDecisionEventId) {
+          //       setResetToDecisionEventId(group.resetToDecisionEventId);
+          //     }
+          //   }}
+          //   selected={group.events.some(
+          //     (e) => e.eventId === queryParams.historySelectedEventId
+          //   )}
+          //   workflowCloseStatus={workflowExecutionInfo?.closeStatus}
+          //   workflowIsArchived={workflowExecutionInfo?.isArchived || false}
+          //   workflowCloseTimeMs={workflowCloseTimeMs}
+          // />
+        )}
+        components={{
+          Footer: () => (
+            <WorkflowHistoryTimelineLoadMore
+              error={error}
+              fetchNextPage={fetchMoreEvents}
+              hasNextPage={hasMoreEvents}
+              isFetchingNextPage={isFetchingMoreEvents}
+            />
+          ),
+        }}
+      />
     </>
   );
 }
--- src/views/workflow-history-v2/workflow-history-v2.types.ts
+++ src/views/workflow-history-v2/workflow-history-v2.types.ts
@@ -2,8 +2,9 @@ import { type WorkflowPageTabContentProps } from '../workflow-page/workflow-page
 
 export type Props = WorkflowPageTabContentProps;
 
-export type WorkflowHistoryEventFilteringTypeColors = {
-  content: string;
-  background: string;
-  backgroundHighlighted: string;
+export type VisibleHistoryRanges = {
+  groupedStartIndex: number;
+  groupedEndIndex: number;
+  ungroupedStartIndex: number;
+  ungroupedEndIndex: number;
 };
