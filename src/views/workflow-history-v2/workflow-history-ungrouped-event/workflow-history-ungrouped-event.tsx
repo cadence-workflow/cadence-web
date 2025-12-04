7369e168430eadb3e5fe4dc20941a4b063bf3763
Adhitya Mamallan <adhitya.mamallan@uber.com>
2025-12-04 20:59:19 +0100

    more changes to ungrouped event
    Signed-off-by: Adhitya Mamallan <adhitya.mamallan@uber.com>

--- src/views/workflow-history-v2/workflow-history-ungrouped-event/workflow-history-ungrouped-event.tsx
+++ src/views/workflow-history-v2/workflow-history-ungrouped-event/workflow-history-ungrouped-event.tsx
@@ -1,8 +1,143 @@
-import { styled } from './workflow-history-ungrouped-event.styles';
+import { useCallback, useMemo } from 'react';
+
+import { Panel } from 'baseui/accordion';
+import { MdOutlineCircle } from 'react-icons/md';
+
+import formatDate from '@/utils/data-formatters/format-date';
+import parseGrpcTimestamp from '@/utils/datetime/parse-grpc-timestamp';
+import WorkflowHistoryEventStatusBadge from '@/views/workflow-history/workflow-history-event-status-badge/workflow-history-event-status-badge';
+import WorkflowHistoryGroupLabel from '@/views/workflow-history/workflow-history-group-label/workflow-history-group-label';
+import WorkflowHistoryTimelineResetButton from '@/views/workflow-history/workflow-history-timeline-reset-button/workflow-history-timeline-reset-button';
+
+import workflowHistoryEventFilteringTypeColorsConfig from '../config/workflow-history-event-filtering-type-colors.config';
+import generateHistoryGroupDetails from '../helpers/generate-history-group-details';
+import WorkflowHistoryDetailsRow from '../workflow-history-details-row/workflow-history-details-row';
+import getEventGroupFilteringType from '../workflow-history-event-group/helpers/get-event-group-filtering-type';
+import getFormattedEventsDuration from '../workflow-history-event-group-duration/helpers/get-formatted-events-duration';
+import WorkflowHistoryGroupDetails from '../workflow-history-group-details/workflow-history-group-details';
+
+import {
+  styled,
+  overrides as getOverrides,
+} from './workflow-history-ungrouped-event.styles';
 import { type Props } from './workflow-history-ungrouped-event.types';
 
-export default function WorkflowHistoryUngroupedEvent({ eventInfo }: Props) {
+export default function WorkflowHistoryUngroupedEvent({
+  eventInfo,
+  animateOnEnter,
+  workflowStartTimeMs,
+  onReset,
+  decodedPageUrlParams,
+  isExpanded,
+  toggleIsExpanded,
+}: Props) {
+  const eventFilteringType = getEventGroupFilteringType(eventInfo.eventGroup);
+
+  const overrides = getOverrides(eventFilteringType, animateOnEnter);
+
+  const handleReset = useCallback(() => {
+    if (onReset) {
+      onReset();
+    }
+  }, [onReset]);
+
+  const { groupDetailsEntries, summaryDetailsEntries } = useMemo(
+    () => generateHistoryGroupDetails(eventInfo.eventGroup),
+    [eventInfo.eventGroup]
+  );
+
+  const eventSummaryDetails = summaryDetailsEntries.find(
+    ([eventId]) => eventId === eventInfo.id
+  )?.[1].eventDetails;
+
   return (
-    <styled.TempContainer>{JSON.stringify(eventInfo)}</styled.TempContainer>
+    <Panel
+      title={
+        <styled.HeaderContent>
+          <MdOutlineCircle
+            color={
+              workflowHistoryEventFilteringTypeColorsConfig[eventFilteringType]
+                .content
+            }
+          />
+          <styled.HeaderLabel>{eventInfo.id}</styled.HeaderLabel>
+          <styled.HeaderLabel>
+            <WorkflowHistoryGroupLabel
+              label={eventInfo.label}
+              shortLabel={eventInfo.shortLabel}
+            />
+          </styled.HeaderLabel>
+          <styled.StatusContainer>
+            <WorkflowHistoryEventStatusBadge
+              statusReady={true}
+              size="small"
+              status={eventInfo.eventMetadata.status}
+            />
+            {eventInfo.eventMetadata.label}
+          </styled.StatusContainer>
+          <div>
+            {eventInfo.event.eventTime
+              ? formatDate(parseGrpcTimestamp(eventInfo.event.eventTime))
+              : null}
+          </div>
+          <div>
+            {eventInfo.event.eventTime && workflowStartTimeMs
+              ? getFormattedEventsDuration(
+                  workflowStartTimeMs,
+                  parseGrpcTimestamp(eventInfo.event.eventTime)
+                )
+              : null}
+          </div>
+          <styled.SummarizedDetailsContainer>
+            {eventSummaryDetails && eventSummaryDetails.length > 0 ? (
+              <WorkflowHistoryDetailsRow
+                detailsEntries={eventSummaryDetails}
+                {...decodedPageUrlParams}
+              />
+            ) : (
+              <div />
+            )}
+          </styled.SummarizedDetailsContainer>
+          <styled.ActionsContainer>
+            {eventInfo.canReset && (
+              <WorkflowHistoryTimelineResetButton
+                workflowId={decodedPageUrlParams.workflowId}
+                runId={decodedPageUrlParams.runId}
+                domain={decodedPageUrlParams.domain}
+                cluster={decodedPageUrlParams.cluster}
+                onReset={handleReset}
+              />
+            )}
+          </styled.ActionsContainer>
+        </styled.HeaderContent>
+      }
+      expanded={isExpanded}
+      onChange={toggleIsExpanded}
+      overrides={overrides.panel}
+    >
+      <styled.GroupDetailsGridContainer>
+        <styled.GroupDetailsNameSpacer />
+        <styled.GroupDetailsContainer>
+          <WorkflowHistoryGroupDetails
+            groupDetailsEntries={[
+              [
+                `summary_${eventInfo.eventGroup.firstEventId}`,
+                {
+                  eventDetails: summaryDetailsEntries.flatMap(
+                    ([_, eventSummaryDetails]) =>
+                      eventSummaryDetails.eventDetails
+                  ),
+                  eventLabel: 'Summary',
+                },
+              ],
+              ...groupDetailsEntries,
+            ]}
+            initialEventId={eventInfo.id}
+            workflowPageParams={decodedPageUrlParams}
+            onClose={() => toggleIsExpanded()}
+          />
+        </styled.GroupDetailsContainer>
+      </styled.GroupDetailsGridContainer>
+    </Panel>
   );
 }
