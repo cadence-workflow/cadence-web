da4e2b397fede89a9b40c2773c63601ad2e44c19
Adhitya Mamallan <adhitya.mamallan@uber.com>
2025-12-12 13:51:40 +0100

    Modify styles
    Signed-off-by: Adhitya Mamallan <adhitya.mamallan@uber.com>

--- src/views/workflow-history-v2/workflow-history-event-group-duration/workflow-history-event-group-duration.styles.ts
+++ src/views/workflow-history-v2/workflow-history-event-group-duration/workflow-history-event-group-duration.styles.ts
@@ -3,7 +3,7 @@ import { styled as createStyled, type Theme } from 'baseui';
 export const styled = {
   DurationContainer: createStyled('div', ({ $theme }: { $theme: Theme }) => ({
     display: 'flex',
-    alignItems: 'center',
-    gap: $theme.sizing.scale100,
+    alignItems: 'baseline',
+    gap: $theme.sizing.scale200,
   })),
 };
