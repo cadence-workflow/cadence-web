d0c6e864ddca8542fce031156aa0b35472c2d9fc
Ender Demirkaya <enderd@uber.com>
2026-01-06 13:55:00 -0800

    Move docs under home
    Signed-off-by: Ender Demirkaya <enderd@uber.com>

--- src/app/(Home)/docs/page.tsx
+++ src/app/(Home)/docs/page.tsx
@@ -1,21 +1,12 @@
 'use client';
-import { HeadingMedium } from 'baseui/typography';
-import Link from 'next/link';
-
 import Markdown from '@/components/markdown/markdown';
 import PageSection from '@/components/page-section/page-section';
 
 import content from './content/markdown-guide';
-import { styled } from './page.styles';
 
 export default function DocsPage() {
   return (
     <PageSection>
-      <styled.Header>
-        <Link href="/">
-          <HeadingMedium>Cadence</HeadingMedium>
-        </Link>
-      </styled.Header>
       <Markdown markdown={content} />
     </PageSection>
   );
