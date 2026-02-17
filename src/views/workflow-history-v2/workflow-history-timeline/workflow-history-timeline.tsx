e2b0f0cd840c4764caf95f5b39e03974a6a5cbd8
Adhitya Mamallan <adhitya.mamallan@uber.com>
2026-02-17 15:33:55 +0100

    Fix nits
    Signed-off-by: Adhitya Mamallan <adhitya.mamallan@uber.com>

--- src/views/workflow-history-v2/workflow-history-timeline/workflow-history-timeline.tsx
+++ src/views/workflow-history-v2/workflow-history-timeline/workflow-history-timeline.tsx
@@ -67,7 +67,8 @@ export default function WorkflowHistoryTimeline({
   const isWorkflowCompleted = workflowCloseTimeMs != null;
 
   const currentTimeMs = useCurrentTimeMs({
-    isWorkflowRunning: !isWorkflowCompleted,
+    isWorkflowRunning:
+      workflowCloseTimeMs === null || workflowCloseTimeMs === undefined,
   });
 
   const currentTimeOffsetMs = useMemo(
@@ -188,7 +189,7 @@ export default function WorkflowHistoryTimeline({
               initialTopMostItemIndex={maybeHighlightedRowIndex}
               itemContent={(index, row) => {
                 const isEven = index % 2 === 0;
-                const isRunning = row.group.hasMissingEvents;
+                const isRunning = row.group.hasMissingEvents ?? false;
                 const color =
                   workflowHistoryEventFilteringTypeColorsConfig[row.groupType]
                     .content;
