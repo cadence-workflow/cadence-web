9075d74890ee1ab18e0ba3058e4772e34033923a
Adhitya Mamallan <adhitya.mamallan@uber.com>
2026-02-24 17:13:12 +0100

    remove date filter v2
    Signed-off-by: Adhitya Mamallan <adhitya.mamallan@uber.com>

--- src/components/date-filter-v2/helpers/is-relative-date-filter-value.ts
+++
@@ -1,8 +0,0 @@
-import { DATE_FILTER_RELATIVE_VALUES } from '../date-filter-v2.constants';
-import { type RelativeDateFilterValue } from '../date-filter-v2.types';
-
-export default function isRelativeDateFilterValue(
-  v: any
-): v is RelativeDateFilterValue {
-  return Object.hasOwn(DATE_FILTER_RELATIVE_VALUES, v);
-}
