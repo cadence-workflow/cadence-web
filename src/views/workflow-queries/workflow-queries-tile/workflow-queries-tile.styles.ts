c0ace2daa0143c4915078069d6d3499b911eaeca
Adhitya Mamallan <adhitya.mamallan@uber.com>
2026-03-18 13:58:08 +0100

    Show full query label name
    Signed-off-by: Adhitya Mamallan <adhitya.mamallan@uber.com>

--- src/views/workflow-queries/workflow-queries-tile/workflow-queries-tile.styles.ts
+++ src/views/workflow-queries/workflow-queries-tile/workflow-queries-tile.styles.ts
@@ -34,15 +34,15 @@ export const styled = {
   }),
   LabelContainer: createStyled('div', ({ $theme }) => ({
     display: 'flex',
+    alignItems: 'center',
     gap: $theme.sizing.scale300,
   })),
   Label: createStyled('div', ({ $theme }) => ({
     ...$theme.typography.MonoLabelSmall,
     paddingLeft: $theme.sizing.scale300,
-    maxWidth: '180px',
-    overflow: 'hidden',
-    textOverflow: 'ellipsis',
-    whiteSpace: 'nowrap',
+    maxWidth: '240px',
+    whiteSpace: 'wrap',
+    wordBreak: 'break-all',
   })),
   Actions: createStyled('div', ({ $theme }) => ({
     display: 'flex',
