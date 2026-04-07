af431d658b3a6e5692eee9ef9d16c6dad34c1649
Adhitya Mamallan <adhitya.mamallan@uber.com>
2026-04-07 14:45:04 +0200

    Implement workflows list grid
    Signed-off-by: Adhitya Mamallan <adhitya.mamallan@uber.com>

--- src/views/shared/workflows-list/helpers/get-workflows-list-column-from-search-attribute.ts
+++ src/views/shared/workflows-list/helpers/get-workflows-list-column-from-search-attribute.ts
@@ -1,10 +1,11 @@
 import { type IndexedValueType } from '@/__generated__/proto-ts/uber/cadence/api/v1/IndexedValueType';
 import { SYSTEM_SEARCH_ATTRIBUTES } from '@/route-handlers/get-search-attributes/get-search-attributes.constants';
-import formatPayload from '@/utils/data-formatters/format-payload';
 
 import workflowsListColumnsConfig from '../config/workflows-list-columns.config';
 import { DEFAULT_WORKFLOWS_LIST_COLUMN_WIDTH } from '../workflows-list.constants';
 import { type WorkflowsListColumn } from '../workflows-list.types';
+
+import getSearchAttributeValue from './get-search-attribute-value';
 
 export default function getWorkflowsListColumnFromSearchAttribute(
   attributeName: string,
@@ -20,12 +21,14 @@ export default function getWorkflowsListColumnFromSearchAttribute(
 
   return {
     id: attributeName,
-    name: config?.name ?? attributeName,
+    name: config?.name ?? (isSystem ? attributeName : `*${attributeName}`),
     width: config?.width ?? DEFAULT_WORKFLOWS_LIST_COLUMN_WIDTH,
     isSystem,
     renderCell: config
       ? (row) => config.renderCell(row, attributeName)
-      : (row) =>
-          String(formatPayload(row.searchAttributes?.[attributeName]) ?? ''),
+      : (row) => {
+          const value = getSearchAttributeValue(row, attributeName);
+          return value == null ? null : String(value);
+        },
   };
 }
--- src/views/shared/workflows-list/workflows-list.tsx
+++ src/views/shared/workflows-list/workflows-list.tsx
@@ -1,3 +1,72 @@
-export default function WorkflowsList() {
-  return <div>Placeholder for new Workflows List</div>;
+import { useMemo } from 'react';
+
+import NextLink from 'next/link';
+
+import TableInfiniteScrollLoader from '@/components/table/table-infinite-scroll-loader/table-infinite-scroll-loader';
+
+import { styled } from './workflows-list.styles';
+import { type Props } from './workflows-list.types';
+
+export default function WorkflowsList({
+  workflows,
+  columns,
+  isLoading,
+  error,
+  hasNextPage,
+  fetchNextPage,
+  isFetchingNextPage,
+}: Props) {
+  const gridTemplateColumns = useMemo(
+    () => columns.map((col) => col.width).join(' '),
+    [columns]
+  );
+
+  const shouldShowResults = !isLoading && workflows.length > 0;
+
+  return (
+    <div>
+      <styled.ScrollArea>
+        {/* TODO @adhitya.mamallan - add a scroll shadow here */}
+        <styled.Container>
+          <styled.GridHeader $gridTemplateColumns={gridTemplateColumns}>
+            {columns.map((col) => (
+              <styled.HeaderCell key={col.id}>{col.name}</styled.HeaderCell>
+            ))}
+          </styled.GridHeader>
+          {shouldShowResults &&
+            workflows.map((workflow, index) => (
+              <styled.GridRow
+                key={`${workflow.workflowID}-${workflow.runID}-${index}`}
+                $as={NextLink}
+                href={`workflows/${encodeURIComponent(workflow.workflowID)}/${encodeURIComponent(workflow.runID)}`}
+                prefetch={false}
+                $gridTemplateColumns={gridTemplateColumns}
+              >
+                {columns.map((col) => {
+                  const content = col.renderCell(workflow);
+                  return (
+                    <styled.GridCell key={col.id}>
+                      {content !== null ? (
+                        content
+                      ) : (
+                        <styled.CellPlaceholder>None</styled.CellPlaceholder>
+                      )}
+                    </styled.GridCell>
+                  );
+                })}
+              </styled.GridRow>
+            ))}
+        </styled.Container>
+      </styled.ScrollArea>
+      <styled.FooterContainer>
+        <TableInfiniteScrollLoader
+          hasData={workflows.length > 0}
+          error={error}
+          fetchNextPage={fetchNextPage}
+          hasNextPage={hasNextPage}
+          isFetchingNextPage={isFetchingNextPage}
+        />
+      </styled.FooterContainer>
+    </div>
+  );
 }
