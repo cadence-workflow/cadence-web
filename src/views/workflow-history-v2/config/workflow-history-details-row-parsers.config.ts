f9b7e9ababdf33f3b39a076c7b1a0c76c6227642
Assem Hafez <assem.hafez@uber.com>
2026-04-22 21:39:08 +0200

    make retries more visible

--- src/views/workflow-history-v2/config/workflow-history-details-row-parsers.config.ts
+++ src/views/workflow-history-v2/config/workflow-history-details-row-parsers.config.ts
@@ -36,8 +36,10 @@ const workflowHistoryDetailsRowParsersConfig: Array<DetailsRowItemParser> = [
     name: '"attempt" greater than 0, as "retries"',
     matcher: (name) => name === 'attempt',
     hide: (_, value) => typeof value === 'number' && value <= 0,
-    icon: MdReplay,
-    customTooltipContent: () => 'retries',
+    icon: null,
+    customRenderValue: ({ value }) =>
+      value === 1 ? '1 Retry' : `${value} Retries`,
+    badgeColor: 'warning',
   },
   {
     name: 'Workflow links as having clickable content',
