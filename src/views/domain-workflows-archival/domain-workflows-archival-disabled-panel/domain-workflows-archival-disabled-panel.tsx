6a86e8728e4270a0f86a3fac016a57080cb808a6
Adhitya Mamallan <adhitya.mamallan@uber.com>
2026-06-23 15:20:29 +0200

    fix lint
    Signed-off-by: Adhitya Mamallan <adhitya.mamallan@uber.com>

--- src/views/domain-workflows-archival/domain-workflows-archival-disabled-panel/domain-workflows-archival-disabled-panel.tsx
+++ src/views/domain-workflows-archival/domain-workflows-archival-disabled-panel/domain-workflows-archival-disabled-panel.tsx
@@ -3,7 +3,6 @@ import ErrorPanel from '@/components/error-panel/error-panel';
 import domainWorkflowsArchivalDisabledPanelConfig from '../config/domain-workflows-archival-disabled-panel.config';
 
 export default function DomainWorkflowsArchivalDisabledPanel() {
-  // TODO: Replace with ErrorPanel
   return (
     <ErrorPanel
       message={domainWorkflowsArchivalDisabledPanelConfig.title}
@@ -17,31 +16,4 @@ export default function DomainWorkflowsArchivalDisabledPanel() {
       )}
     />
   );
-  // return (
-  //   <PanelSection>
-  //     <Image width={64} alt="Error" src={errorIcon} />
-  //     <styled.Title>
-  //       {domainWorkflowsArchivalDisabledPanelConfig.title}
-  //     </styled.Title>
-  //     <styled.Content>
-  //       {domainWorkflowsArchivalDisabledPanelConfig.details.map(
-  //         (detail, index) => (
-  //           <styled.Detail key={`details-${index}`}>{detail}</styled.Detail>
-  //         )
-  //       )}
-  //     </styled.Content>
-  //     <styled.LinksContainer>
-  //       {domainWorkflowsArchivalDisabledPanelConfig.links.map(
-  //         ({ text, href }) => (
-  //           <styled.LinkContainer key={`link-${href}`}>
-  //             <Link href={href} target="_blank" rel="noreferrer">
-  //               {text}
-  //             </Link>
-  //             <MdOpenInNew />
-  //           </styled.LinkContainer>
-  //         )
-  //       )}
-  //     </styled.LinksContainer>
-  //   </PanelSection>
-  // );
 }
