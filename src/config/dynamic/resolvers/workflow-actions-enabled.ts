3a2ae4dec0f5ff1ec83f8e347aa381095c015461
Assem Hafez <assem.hafez@uber.com>
2026-07-01 10:21:08 +0200

    update resolvers comments

--- src/config/dynamic/resolvers/workflow-actions-enabled.ts
+++ src/config/dynamic/resolvers/workflow-actions-enabled.ts
@@ -13,7 +13,8 @@ import {
  * Override this resolver if you have different
  * requirements for enabling/disabling workflow actions.
  *
- * All workflow actions are enabled by default for users with write access to the domain.
+ * All workflow actions are enabled by default if authorization is disabled.
+ * Otherwise it is only enabled for users with write access to domain.
  * Domain access is resolved via the DOMAIN_ACCESS config resolver.
  */
 export default async function workflowActionsEnabled(
