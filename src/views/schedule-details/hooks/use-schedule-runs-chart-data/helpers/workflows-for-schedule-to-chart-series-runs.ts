20cfcf9bc419e9bf97a6c13abacde77c20510ba4
Assem Hafez <137278762+Assem-Uber@users.noreply.github.com>
2026-08-03 15:08:13 +0200

    fix date parsing

--- src/views/schedule-details/hooks/use-schedule-runs-chart-data/helpers/workflows-for-schedule-to-chart-series-runs.ts
+++ src/views/schedule-details/hooks/use-schedule-runs-chart-data/helpers/workflows-for-schedule-to-chart-series-runs.ts
@@ -6,21 +6,22 @@ import { type ChartSeriesRun } from '@/views/schedule-details/schedule-details-r
 import { SCHEDULE_WORKFLOWS_VISIBILITY_SORT_COLUMN } from '@/views/schedule-details/schedule-details.constants';
 import getSearchAttributeValue from '@/views/shared/workflows-list/helpers/get-search-attribute-value';
 
-import parseScheduleSearchAttributeTimeMs from './parse-schedule-search-attribute-time-ms';
-
 export default function workflowsForScheduleToChartSeriesRuns(
   data: InfiniteData<ListWorkflowsResponse> | undefined
 ): ChartSeriesRun[] {
   return (data?.pages.flatMap((page) => page.workflows ?? []) ?? []).reduce<
     ChartSeriesRun[]
   >((runs, workflow) => {
-    const scheduledTimeMs = parseScheduleSearchAttributeTimeMs(
-      getSearchAttributeValue(
-        workflow,
-        SCHEDULE_WORKFLOWS_VISIBILITY_SORT_COLUMN
-      )
+    const scheduleTime = getSearchAttributeValue(
+      workflow,
+      SCHEDULE_WORKFLOWS_VISIBILITY_SORT_COLUMN
     );
-    if (scheduledTimeMs == null) {
+    if (typeof scheduleTime !== 'string') {
+      return runs;
+    }
+
+    const scheduledTimeMs = Date.parse(scheduleTime);
+    if (!Number.isFinite(scheduledTimeMs)) {
       return runs;
     }
 
